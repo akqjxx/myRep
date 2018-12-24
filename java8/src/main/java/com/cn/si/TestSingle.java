835358872b25c44b488e1bb83d64ee129eb9f428
akqjxx <liuer2595919@163.com>
2018-12-24 17:15:24 +0800

    提交 Change-Id: I3ab2c9a84d8099a91faa45709fb622c03c788e92 Signed-off-by: akqjxx <liuer2595919@163.com>

--- java8/src/main/java/com/cn/si/TestSingle.java
+++ java8/src/main/java/com/cn/si/TestSingle.java
@@ -1,33 +1,25 @@
 package com.cn.si;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.junit.Test;
 
 public class TestSingle {
-	
-	
+
 	@Test
 	public void test() throws InterruptedException, ExecutionException {
-		
-		Set<Single> set = new HashSet<>();
-		Callable<Single> task =()->{
-			Single s = Single.createSingle();
-			return s;
-		};
-		java.util.concurrent.ExecutorService  es = Executors.newFixedThreadPool(10);
-		for(int i =0 ;i<10000;i++) {
-			Future<Single> f  =  es.submit(task);
-			set.add(f.get());
+		Set<Single> set = new LinkedHashSet<>() ;
+		Callable<Single> task = () -> Single.createSingle();
+		ExecutorService es = Executors.newFixedThreadPool(10);
+		for (int i = 0; i < 100000; i++) {
+			set.add(es.submit(task).get());
 		}
 		es.shutdown();
-		
 		set.forEach(System.out::println);
 	}
-
 }
